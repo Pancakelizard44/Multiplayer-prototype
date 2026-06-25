e10ec2956ea0656e945f2056eb652a714e0e3197
Pancakelizard44 <25GraingerEdward@colchsfc.ac.uk>
2026-06-25 11:32:34 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -9,6 +9,19 @@ function setup() {
 
 function initLevel(){
         cellSize = floor(min(width / (mapSize + 2), height / (mapSize + 2)));
+        
+    //0=floor 1=walltop 2=wallside
+    map = [
+        [1, 1, 1, 0, 0, 0, 1, 1, 1],
+        [2, 2, 2, 0, 0, 0, 2, 2, 2],
+        [2, 2, 2, 0, 0, 0, 2, 2, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 1, 1, 1],
+    ]
 }
 
 let players = {};
@@ -60,12 +73,34 @@ document.addEventListener("keyup", (e) => {
 
 // Draw loop
 function draw() {
-
    background(200)
     
     for (let id in players) {
         let p = players[id];
             fill("red")
-        square(p.x * cellSize, p.y * cellSize, 20);
+        square(p.x * cellSize, p.y * cellSize, cellSize);
+    }
+    console.log(id)
+}
+
+function drawWorld() {
+    for (let i = 0; i <= 8; i++) {
+        for (let j = 0; j <= 8; j++) {
+            switch (map[i][j]) {
+                case 0:
+                    fill(200);
+                    break;
+                case 1:
+                    fill(50);
+                    break;
+                case 2:
+                    fill("gray");
+                    break;
+                default:
+                    console.log("ERROR the map is not loading properly")
+                    break;
+            }
+            square(j * cellSize, i * cellSize, cellSize)
+        }
     }
 }
