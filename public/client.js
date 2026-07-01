092417051ff5b73b9ff699c8099721521a896782
Pancakelizard44 <25GraingerEdward@colchsfc.ac.uk>
2026-07-01 13:08:09 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -45,10 +45,10 @@ socket.on("updatePlayers", (serverPlayers) => {
 // Movement
 document.addEventListener("keydown", (e) => {
 
-    if (e.key === "w" || e.key === "UP_ARROW") {inputY = -1}
-    if (e.key === "s" || e.key === "DOWN_ARROW") {inputY = 1}
-    if (e.key === "a" || e.key === "LEFT_ARROW") {inputX = -1}
-    if (e.key === "d" || e.key === "RIGHT_ARROW") {inputX = 1}
+    if (e.key === "w" || e.key === "ArrowUp") {inputY = -1}
+    if (e.key === "s" || e.key === "ArrowDown") {inputY = 1}
+    if (e.key === "a" || e.key === "ArrowLeft") {inputX = -1}
+    if (e.key === "d" || e.key === "ArrowRight") {inputX = 1}
 
     socket.emit("moveY", inputY);
     socket.emit("moveX", inputX);
@@ -56,10 +56,10 @@ document.addEventListener("keydown", (e) => {
 
 document.addEventListener("keyup", (e) => {
 
-    if ((e.key === "w" || e.key === "UP_ARROW") && inputY < 0) {inputY = 0}
-    if ((e.key === "s" || e.key === "DOWN_ARROW") && inputY > 0) {inputY = 0}
-    if ((e.key === "a" || e.key === "LEFT_ARROW") && inputX < 0) {inputX = 0}
-    if ((e.key === "d" || e.key === "RIGHT_ARROW") && inputX > 0) {inputX = 0}
+    if ((e.key === "w" || e.key === "ArrowUp") && inputY < 0) {inputY = 0}
+    if ((e.key === "s" || e.key === "ArrowDown") && inputY > 0) {inputY = 0}
+    if ((e.key === "a" || e.key === "ArrowLeft") && inputX < 0) {inputX = 0}
+    if ((e.key === "d" || e.key === "ArrowRight") && inputX > 0) {inputX = 0}
 
     socket.emit("moveY", inputY);
     socket.emit("moveX", inputX);
