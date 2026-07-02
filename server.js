4f4ca98f5eecc7b1b801943858fa3b788cb4ea24
Pancakelizard44 <25GraingerEdward@colchsfc.ac.uk>
2026-07-02 13:12:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -30,15 +30,19 @@ class serverPlayer {
         checkTarget(){
                 if(this.xv > 0 && (map(Math.round(player.x) + 1, Math.round(player.y)) === 1 || map(Math.round(player.x) + 1,Math.round(player.y)) === 2)) {
                         this.xv = 0
+                                        console.log("player ",this.id, " has colided with a wall")
                 } else if(this.yx < 0 && (map(Math.round(player.x) - 1,  Math.round(player.y)) === 1 || map(Math.round(player.x) - 1, Math.round(player.y)) === 2)) {
                         this.xv = 0
+                                        console.log("player ",this.id, " has colided with a wall")
                 }
                 if(this.xv > 0 && (map(Math.round(player.x), Math.round(player.y) + 1) === 1 || map(Math.round(player.x),Math.round(player.y) + 1) === 2)) {
                         this.yv = 0
+                                        console.log("player ",this.id, " has colided with a wall")
                 } else if(this.yx < 0 && (map(Math.round(player.x),  Math.round(player.y) - 1) === 1 || map(Math.round(player.x), Math.round(player.y) - 1) === 2)) {
                         this.yv = 0
+                                        console.log("player ",this.id, " has colided with a wall")
                 }
-                console.log("player ",this.id, " has colided with a wall")
+
         }
 }
 
